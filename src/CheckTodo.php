c6f739f4353666c54fe0c9e67d14778951650dbe
K-Shinhira <144861653+K-Shinhira@users.noreply.github.com>
2024-02-06 11:39:29 +0900

    変更

--- src/CheckTodo.php
+++ src/CheckTodo.php
@@ -14,6 +14,7 @@
                     <div class="mb-3">
                         <label for="evaluation" class="col-form-label">評価を選択してください。</label>
                         <input type="range" class="form-range" min="0" max="5" step="0.5" id="evaluation">
+<!--
                         <datalist id="values">
                             <option value="0" label="1"></option>
                             <option value="25" label="2"></option>
@@ -21,6 +22,8 @@
                             <option value="75" label="4"></option>
                             <option value="100" label="5"></option>
                         </datalist>
+-->
+                        <h4>☆<span id="current-value"></span></h4>
                     </div>
                 </form>
             </div>
@@ -56,5 +59,27 @@ if (CheckTodoModal) {
         location.href = "complete.php?id="+data1+"&rating="+rating
     })
   })
+    // input要素
+    const inputElem = document.getElementById('evaluation');
+
+    // 埋め込む先の要素
+    const currentValueElem = document.getElementById('current-value');
+
+    // 現在の値を埋め込む関数
+    const setCurrentValue = (val) => {
+        currentValueElem.innerText = val;
+    }
+
+    // inputイベント時に値をセットする関数
+    const rangeOnChange = (e) =>{
+        setCurrentValue(e.target.value);
+    }
+
+    window.onload = () => {
+    // 変更に合わせてイベントを発火する
+    inputElem.addEventListener('input', rangeOnChange);
+    // ページ読み込み時の値をセット
+    setCurrentValue(inputElem.value);
+    }
 }
 </script>
